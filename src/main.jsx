42e63c9d8d3fc50337d23896674fece8c76a10a6
suraj6854 <surajhm6854@outlook.com>
2025-02-06 17:50:40 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
 import App from './App'
-import { BrowserRouter, Routes, Route } from "react"
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>  
-<BrowserRouter basename="/Brandpage/"></BrowserRouter>
+
    <App/>
   </StrictMode>,
 )
